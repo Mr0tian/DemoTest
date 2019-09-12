86d409d9441159b355e12138727c7d284be57e1e
tian <596893301@qq.com>
2019-09-12 09:29:17 +0800

    修改

--- myutil/src/main/java/com/example/daotest/DaoUtil.java
+++ myutil/src/main/java/com/example/daotest/DaoUtil.java
@@ -13,6 +13,7 @@ import java.util.List;
  */
 public class DaoUtil {
 
+    //sa
     public TestBeanDao getTestBeanDao(){
         return GreenDaoManager.getInstance().getmDaoSession().getTestBeanDao();
     }
